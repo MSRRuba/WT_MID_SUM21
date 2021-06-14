66519dfb733dd15a3eb41e915a5fb8b7045dd5e3
MSRRuba <84693918+MSRRuba@users.noreply.github.com>
2021-06-14 11:11:39 +0600

    Add files via upload

---
+++ Lab3/5.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+    <body>
+        <?php
+$backColor="green"; $fontColor="red"; $imageWidth="80px"; $imageHeight="80px";
+?>
+
+<table>
+    <tr>
+        <td><img src="aiub_logo.png" width=<?php echo "$imageWidth" ?>; height=<?php echo"$imageHeight"?> /></td>
+        <td><h2>American International <br/>University -<span style="background-color:<?php echo "$backColor";?>;color:<?php echo"$fontColor";?>">
+        Bangladesh</h2>
+</span>
+</td>
+
+    </tr>
+</table>
+
+
+    </body>
+</html>
